f9a1ba2d8ccae72b8d1577bff6d104095ba842cb
Alex Buly <asbuly@icloud.com>
2024-03-18 13:29:19 -0400

    Adds initial logic and sets up objects and arrays

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+This game will have a board, 2 players. The game will switch between players after their turn. When
+3 in a row is achieved, the player wins. If no player wins, a tie occurs. 
+ */
+
+// gameboard object 
+const Gameboard = {
+    gameBoard: [["_", "_", "_"], ["_", "_", "_"], ["_","_","_"]]
+}
+console.log(Gameboard.gameBoard);
+
+// players 
+const players = {
+    player1: "X",
+    player2: "O",
+}
+
+console.log(players);
+
+
