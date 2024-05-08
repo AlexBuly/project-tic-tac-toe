e0dc5358cea4c61c452dbb603fef9a9c69e1d5f2
Alex Buly <asbuly@icloud.com>
2024-05-07 22:14:41 -0400

    Can add values to the first column

--- script.js
+++ script.js
@@ -7,10 +7,39 @@ This game will have a board, 2 players. The game will switch between players aft
 const Gameboard = {
     gameBoard: [["_","_","_"],["_", "_", "_"], ["_","_","_"]]
 }
-const insert = (start, del, a, b ,c) => {
-    Gameboard.gameBoard.splice(start, del, [a, b ,c]);
-    if (Gameboard.gameBoard[1] == ["X","X","X"]) {
-        console.log("X wins!");
+const insert = (row, col, val) => {
+    if (row == 0) {
+        if (col == 0) {
+            Gameboard.gameBoard[0].splice(0, 1, val);
+        }
+        if (col == 1) {
+            Gameboard.gameBoard[0].splice(0, 1, val);
+        }
+        if (col = 2) {
+            Gameboard.gameBoard[0].splice(0, 1, val);
+        }
+    }
+    if (row == 1) {
+        if (col == 0) {
+            Gameboard.gameBoard[1].splice(0, 1, val);
+        }
+        if (col == 1) {
+            Gameboard.gameBoard[1].splice(0, 1, val);
+        }
+        if (col = 2) {
+            Gameboard.gameBoard[1].splice(0, 1, val);
+        }
+    }
+    if (row == 2) {
+        if (col == 0) {
+            Gameboard.gameBoard[2].splice(0, 1, val);
+        }
+        if (col == 1) {
+            Gameboard.gameBoard[2].splice(0, 1, val);
+        }
+        if (col = 2) {
+            Gameboard.gameBoard[2].splice(0, 1, val);
+        }
     }
     return Gameboard.gameBoard;
 }
