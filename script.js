cc2f9ce4c9e92f85814d1e62da562adaa62700a0
Alex Buly <asbuly@icloud.com>
2024-05-08 14:15:08 -0400

    Edits playet object

--- script.js
+++ script.js
@@ -8,52 +8,62 @@ const Gameboard = () => {
     const gameBoard = [["_","_","_"],["_", "_", "_"], ["_","_","_"]];
     console.log(gameBoard);
 
-const insert = (row, col, val) => {
-    if (row == 0) {
-        if (col == 0) {
-           gameBoard[0].splice(0, 1, val);
+    // inserting values 
+    const insert = (row, col, val) => {
+        if (row == 0) {
+            if (col == 0) {
+                gameBoard[0].splice(0, 1, val);
+            }
+            if (col == 1) {
+                gameBoard[0].splice(1, 1, val);
+            }
+            if (col == 2) {
+                gameBoard[0].splice(2, 1, val);
+            }
         }
-        if (col == 1) {
-            gameBoard[0].splice(1, 1, val);
+        if (row == 1) {
+            if (col == 0) {
+                gameBoard[1].splice(0, 1, val);
+            }
+            if (col == 1) {
+                gameBoard[1].splice(1, 1, val);
+            }
+            if (col == 2) {
+                gameBoard[1].splice(2, 1, val);
+            }
         }
-        if (col == 2) {
-            gameBoard[0].splice(2, 1, val);
+        if (row == 2) {
+            if (col == 0) {
+                gameBoard[2].splice(0, 1, val);
+            }
+            if (col == 1) {
+                gameBoard[2].splice(1, 1, val);
+            }
+            if (col == 2) {
+                gameBoard[2].splice(2, 1, val);
+            }
         }
+        return gameBoard;
     }
-    if (row == 1) {
-        if (col == 0) {
-            gameBoard[1].splice(0, 1, val);
-        }
-        if (col == 1) {
-            gameBoard[1].splice(1, 1, val);
-        }
-        if (col == 2) {
-            gameBoard[1].splice(2, 1, val);
-        }
-    }
-    if (row == 2) {
-        if (col == 0) {
-            gameBoard[2].splice(0, 1, val);
-        }
-        if (col == 1) {
-            gameBoard[2].splice(1, 1, val);
-        }
-        if (col == 2) {
-            gameBoard[2].splice(2, 1, val);
-        }
-    }
-    return gameBoard;
-}
+
+    const playerOneName = prompt("Enter player one name")
+    const playerTwoName = prompt("Enter player two name")
 
 
-// players 
-const players = {
-    player1: "X",
-    player2: "O",
-}
+    // players 
+    const players = [
+        {
+            name: playerOneName,
+            token: "X"
+        },
+        {
+            name: playerTwoName,
+            token: "O"
+        }
+    ]
 
-console.log(players);
-return { insert };
+    console.log(players);
+    return { insert };
 }
 
 const game = Gameboard();
