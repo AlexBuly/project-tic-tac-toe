a7145dbad509c1d7f065f38b49733614be9a3fa3
Alex Buly <asbuly@icloud.com>
2024-05-10 21:37:43 -0400

    Finishes game logic without win

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ This game will have a board, 2 players. The game will switch between players aft
  */
 
 // gameboard object 
-const Gameboard = () => {
+const Gameboard = (addToken) => {
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -17,75 +17,42 @@ const Gameboard = () => {
     }
 
     const getBoard = () => board
-    
+
+    // inserting values 
+    const insert = (row, col, player) => {
+        if (board[row][col].getValue() === "_") {
+            board[row][col].addToken(player);
+        }
+    }
+
     const printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
         console.log(boardWithCellValues);
     }
 
-
-    // inserting values 
-    const insert = (row, col, val) => {
-        if (row == 0) {
-            if (col == 0) {
-                board[0].splice(0, 1, val);
-            }
-            if (col == 1) {
-                board[0].splice(1, 1, val);
-            }
-            if (col == 2) {
-                board[0].splice(2, 1, val);
-            }
-        }
-        if (row == 1) {
-            if (col == 0) {
-                board[1].splice(0, 1, val);
-            }
-            if (col == 1) {
-                board[1].splice(1, 1, val);
-            }
-            if (col == 2) {
-                board[1].splice(2, 1, val);
-            }
-        }
-        if (row == 2) {
-            if (col == 0) {
-                board[2].splice(0, 1, val);
-            }
-            if (col == 1) {
-                board[2].splice(1, 1, val);
-            }
-            if (col == 2) {
-                board[2].splice(2, 1, val);
-            }
-        }
-        board[row][col].addToken(val);
-    }
-   
-    printBoard()
-    return { insert, printBoard };
+    return { insert, printBoard, getBoard };
 }
 
 const Cell = () => {
-    let value = "_";
+    let val = "_";
 
     const addToken = (player) => {
-        value = player
+        val = player
     }
 
-    const getValue = () => value;
+    const getValue = () => val;
 
     return {
-        getValue,
-        addToken
-    }
+        addToken,
+        getValue
+    };
 }
 
 const GameController = (
     playerOneName = prompt("Enter player one name:"),
     playerTwoName = prompt("Enter player two name:")
 ) => {
-    const board = Gameboard();
+    const board = Gameboard(/*Cell().addToken*/);
     // players 
     const players = [
         {
@@ -104,15 +71,15 @@ const GameController = (
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
 
-    const getActivePlayer = () => activePlayer
+    const getActivePlayer = () => activePlayer;
 
     const printNewRound = () => {
         board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn.`)
+        console.log(`${getActivePlayer().name}'s turn.`);
     }
 
     const playRound = (row, col) => {
-        board.insert(row, col, getActivePlayer.token());
+        board.insert(row, col, getActivePlayer().token);
 
         switchTurn();
         printNewRound();
@@ -122,9 +89,8 @@ const GameController = (
     return {
         playRound,
         getActivePlayer
-    }
-
+    };
 }
 
-const game = Gameboard();
+const game = GameController();
 
